f285c8a4d61eb45a10ebcd8a4478cf06fcd39e2d
jrodny <devsupport@inglewoodfarms.com>
2023-10-20 14:43:44 +0800

    Add ui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,12 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @can('ModuleEgg')
-               module egg
-            @endcan
+                module egg
+                @endcan
 
-            @can('ModuleRecvEgg')
-             shedfeed
-            @endcan
+                @can('ModuleRecvEgg')
+                shedfeed
+                @endcan
             </div>
         </div>
     </div>
